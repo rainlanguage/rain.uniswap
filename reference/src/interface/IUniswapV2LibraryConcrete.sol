fdc389406441fd86011bd5d95c5264688eae0e07
thedavidmeister <thedavidmeister@gmail.com>
2023-11-27 15:46:35 +0400

    fix build

--- reference/src/interface/IUniswapV2LibraryConcrete.sol
+++ reference/src/interface/IUniswapV2LibraryConcrete.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CAL
-pragma solidity ^0.8.19;
+pragma solidity >=0.6.6;
 
 interface IUniswapV2LibraryConcrete {
     function sortTokens(address tokenA, address tokenB) external pure returns (address token0, address token1);
