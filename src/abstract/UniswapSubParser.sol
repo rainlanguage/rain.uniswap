13036eba6f5824ca460052f4119378f7a9efbdca
thedavidmeister <thedavidmeister@gmail.com>
2024-01-28 12:07:41 +0400

    update docs

--- src/abstract/UniswapSubParser.sol
+++ src/abstract/UniswapSubParser.sol
@@ -52,11 +52,11 @@ function authoringMetaV2() pure returns (bytes memory) {
     );
     meta[SUB_PARSER_WORD_UNISWAP_V2_AMOUNT_OUT] = AuthoringMetaV2(
         "uniswap-v2-amount-out",
-        "Computes the maximum amount of output tokens received from a given amount of input tokens from a UniswapV2 pair. Input/output token directions are from the perspective of the Uniswap contract. The first input is the factory address, the second is the amount of input tokens, the third is the input token address, and the fourth is the output token address. If the operand is 1 the last time the prices changed will be returned as well."
+        "Computes the maximum amount of output tokens received from a given amount of input tokens from a UniswapV2 pair. Input/output token directions are from the perspective of the Uniswap contract. The first input is the input token address, the second is the output token address, and the third is the amount of input tokens. If the operand is 1 the last time the prices changed will be returned as well."
     );
     meta[SUB_PARSER_WORD_UNISWAP_V2_QUOTE] = AuthoringMetaV2(
         "uniswap-v2-quote",
-        "Given an amount of token A, calculates the equivalent valued amount of token B. The first input is the factory address, the second is the amount of token A, the third is token A's address, and the fourth is token B's address. If the operand is 1 the last time the prices changed will be returned as well."
+        "Given an amount of token A, calculates the equivalent valued amount of token B. The first input is token A's address, the second is token B's address and the third is the amount of token A. If the operand is 1 the last time the prices changed will be returned as well."
     );
     meta[SUB_PARSER_WORD_UNISWAP_V3_EXACT_OUTPUT] = AuthoringMetaV2(
         "uniswap-v3-exact-output",
